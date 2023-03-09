e1655dd4660fe4d02bd262c7ef3c04cecfe7d6cb
SuperCoalaMan <89955@glr.nl>
2023-03-09 22:49:08 +0100

    Info page (bijna) af
    Info page bijna af, alleen nog het info tabje laten veranderen. En klein beginnetje aan het account systeem.

--- Info/JS/Info.js
+++ Info/JS/Info.js
@@ -1,14 +1,93 @@
-map = document.getElementById("map");
+let map = document.getElementById("map");
 function locatie(locatie)
 {
     
         if (locatie == "Alkmaar")
         {
             map.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2421.2217409139676!2d4.745194551752699!3d52.637906779737776!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47cf57b236aee9db%3A0x638a44c2d25e0665!2sHappy%20Italy%20Alkmaar!5e0!3m2!1snl!2snl!4v1678368311981!5m2!1snl!2snl";
-
         }
         if (locatie == "Almere")
         {
             map.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2435.9264092036806!2d5.215249651744929!3d52.371754379687246!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c6179ba61c808d%3A0x53464359db9c1f3d!2sHappy%20Italy!5e0!3m2!1snl!2snl!4v1678368935080!5m2!1snl!2snl";
         }
+        if (locatie == "Amsterdam")
+        {
+            map.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2437.761616515034!2d4.891186152091944!3d52.33847057968107!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c60a238bdf7fa5%3A0xfad16c33402c99b5!2sHappy%20Italy%20Amsterdam!5e0!3m2!1sen!2snl!4v1678378285950!5m2!1sen!2snl"
+        }
+        if (locatie == "Arnhem")
+        {
+            map.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2457.252269838099!2d5.902233052084427!3d51.98405547961664!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c7a5b71d4af4db%3A0xb39095e6a5ae4aa2!2sHappy%20Italy%20Arnhem!5e0!3m2!1sen!2snl!4v1678378388886!5m2!1sen!2snl"
+        }
+        if (locatie == "Breda")
+        {
+            map.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2479.3325566649955!2d4.83456235207597!3d51.580468179547225!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c6a1fdcaf967e7%3A0xab1fc4fdd60e06a2!2sHappy%20Italy%20Breda!5e0!3m2!1sen!2snl!4v1678378446927!5m2!1sen!2snl"
+        }
+        if (locatie == "Den-Bosch")
+        {
+            map.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2473.5743229015925!2d5.287594252078157!3d51.685934379565!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c6ee89513c1019%3A0xd5cbe776a8a9163f!2sHappy%20Italy%20Den%20Bosch!5e0!3m2!1sen!2snl!4v1678378505664!5m2!1sen!2snl"
+        }
+        if (locatie == "Eindhoven")
+        {
+            map.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2487.1286020681214!2d5.47815695207297!3d51.437432079523646!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c6d902fdedeb49%3A0x488e45ffcbc05efc!2sHappy%20Italy%20Eindhoven!5e0!3m2!1sen!2snl!4v1678378552176!5m2!1sen!2snl"
+        }
+        if (locatie == "Enschede")
+        {
+            map.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2444.166190543272!2d6.891012952089446!3d52.22219837965951!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b81474830fb075%3A0x956e3d579c0202e2!2sHappy%20Italy%20Enschede!5e0!3m2!1sen!2snl!4v1678378603695!5m2!1sen!2snl"
+        }
+        if (locatie == "Groningen")
+        {
+            map.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2389.065904131247!2d6.568361652110579!3d53.21666667985368!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c9cd56ebc09f0f%3A0x7175dd8863e6ba65!2sHappy%20Italy%20Groningen!5e0!3m2!1sen!2snl!4v1678378840338!5m2!1sen!2snl"
+        }
+        if (locatie == "Hendrik")
+        {
+            map.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2464.9096915989844!2d4.617356474015613!3d51.84434501453258!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c42e806f726e3f%3A0x216f10f373d97e68!2sHappy%20Italy%20Hendrik-Ido-Ambacht!5e0!3m2!1sen!2snl!4v1678378907851!5m2!1sen!2snl"
+        }
+        if (locatie == "Kerkrade")
+        {
+            map.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2518.5749277268897!2d6.000211052060871!3d50.85755527943317!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c097d8a15d89b3%3A0xf0f07842453d09a0!2sHappy%20Italy%20Kerkrade!5e0!3m2!1sen!2snl!4v1678378958053!5m2!1sen!2snl"
+        }
+        if (locatie == "Nieuwegein")
+        {
+            map.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2455.6403610914963!2d5.113695952085023!3d52.01343097962194!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c6657828b8799d%3A0x4203316eeaa3a507!2sHappy%20Italy%20Nieuwegein!5e0!3m2!1sen!2snl!4v1678379039388!5m2!1sen!2snl"
+        }
+        if (locatie == "Roermond")
+        {
+            map.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2500.2408752377387!2d5.983186952067914!3d51.19621307948517!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c74b51b980e93d%3A0xb43db97dd1e72379!2sHappy%20Italy%20Roermond!5e0!3m2!1sen!2snl!4v1678379102450!5m2!1sen!2snl"
+        }
+        if (locatie == "Rotterdamb")
+        {
+            map.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19688.471896743456!2d4.479621714069493!3d51.914634894680276!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c434ac231adf03%3A0x36637546b0fe6aaa!2sHappy%20Italy%20Rotterdam%20Bergweg!5e0!3m2!1sen!2snl!4v1678379196222!5m2!1sen!2snl"
+        }
+        if (locatie == "Rotterdambi")
+        {
+            map.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19688.471896743456!2d4.479621714069493!3d51.914634894680276!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c4335a2d60fa5b%3A0x48c6a8608ae9176b!2sHappy%20Italy%20Rotterdam%20Binnenrotte!5e0!3m2!1sen!2snl!4v1678379283236!5m2!1sen!2snl"
+        }
+        if (locatie == "Rotterdamk")
+        {
+            map.scr = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2462.021991238576!2d4.521308852082563!3d51.89706277960134!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c433018e28cc05%3A0x8059f45918c6081d!2sHappy%20Italy%20Rotterdam%20Zuid!5e0!3m2!1sen!2snl!4v1678379356316!5m2!1sen!2snl"
+        }
+        if (locatie == "Rotterdamz")
+        {
+            map.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19688.471896743453!2d4.4796217140694905!3d51.91463489468029!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c433392a198e3d%3A0x49ecac65cc81d5e5!2sHappy%20Italy%20Rotterdam%20Kop%20van%20Zuid!5e0!3m2!1sen!2snl!4v1678379416386!5m2!1sen!2snl"
+        }
+        if (locatie == "Schiedam")
+        {
+            map.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2460.5437942672907!2d4.404349552083177!3d51.92403397960602!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c43539450c41ed%3A0x9fad603bf28d4bae!2sHappy%20Italy%20Schiedam!5e0!3m2!1sen!2snl!4v1678379481496!5m2!1sen!2snl"
+        }
+        if (locatie == "Spijkernisse")
+        {
+            map.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2464.627488772244!2d4.3226386520815625!3d51.84949857959319!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c449b6e68ff613%3A0x27f512cb31fb2ccb!2sHappy%20Italy%20Spijkenisse!5e0!3m2!1sen!2snl!4v1678379533789!5m2!1sen!2snl"
+        }
+        if (locatie == "Tilburg")
+        {
+            map.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2480.715863825236!2d5.0807323520754055!3d51.555108979543!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c6be3680f3da93%3A0xf1d4a567e8121a7c!2sHappy%20Italy%20Tilburg!5e0!3m2!1sen!2snl!4v1678379575709!5m2!1sen!2snl"
+        }
+        if (locatie == "Venlo")
+        {
+            map.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2490.790554114115!2d6.164931952071511!3d51.37014697951273!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c75b7a651a6ecd%3A0xbda9de0dd9c52719!2sHappy%20Italy%20Venlo!5e0!3m2!1sen!2snl!4v1678379622180!5m2!1sen!2snl"
+        }
+        if (locatie == "Zoetermeer")
+        {
+            map.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2453.034877719628!2d4.491441152086058!3d52.060888579630536!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c5c929812f0805%3A0xfac7c5201b25c6aa!2sHappy%20Italy%20Zoetermeer!5e0!3m2!1sen!2snl!4v1678379682960!5m2!1sen!2snl"
+        }        
 }
