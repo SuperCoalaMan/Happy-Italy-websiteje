4cec8895a32a5647e61a2aefba0c67149bd67113
SuperCoalaMan <89955@glr.nl>
2023-03-09 15:55:44 +0100

    Info page
    Info page verder afgemaakt  en JS toegevoegd.

---
+++ Info/JS/Info.js
@@ -0,0 +1,14 @@
+map = document.getElementById("map");
+function locatie(locatie)
+{
+    
+        if (locatie == "Alkmaar")
+        {
+            map.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2421.2217409139676!2d4.745194551752699!3d52.637906779737776!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47cf57b236aee9db%3A0x638a44c2d25e0665!2sHappy%20Italy%20Alkmaar!5e0!3m2!1snl!2snl!4v1678368311981!5m2!1snl!2snl";
+
+        }
+        if (locatie == "Almere")
+        {
+            map.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2435.9264092036806!2d5.215249651744929!3d52.371754379687246!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c6179ba61c808d%3A0x53464359db9c1f3d!2sHappy%20Italy!5e0!3m2!1snl!2snl!4v1678368935080!5m2!1snl!2snl";
+        }
+}
