f3982ab731b1940c0e9af9452853971008e672b4
Lukaas <122445633+Lukaas123@users.noreply.github.com>
2023-03-13 09:06:10 +0100

    gekke php shit

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,6 +20,18 @@
             <a href="Info/Info.php">info</a>
             <a href="#">bestellen</a>
             <a href="#">menu</a>
+            <?php if (isset($_SESSION['user_id'])) { ?>
+            <a href="../profile/profile.php">Profiel</a>
+            <h1>Welkom terug, <?php echo $_SESSION['user_id']; ?>!</h1>
+        <?php } else { ?>
+          <a href="../AccountGegevens/login.html">Login</a>
+        <?php } ?>
+          <!--Als de user admin is komt er een tabje admin bij-->
+        <?php if ($_SESSION['user_id'] == 'admin') { ?>
+            <a href="../AccountGegevens/admin.php">Admin</a>
+            
+        <?php } else { ?>
+        <?php } ?>
           </nav>
         </div>
       </div>
