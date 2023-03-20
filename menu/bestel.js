550960309008049b91d07f19bd72c1c8b75371d3
Lukaas <122445633+Lukaas123@users.noreply.github.com>
2023-03-20 10:25:23 +0100

    verder met de bestel functie

--- menu/bestel.js
+++ menu/bestel.js
@@ -1,4 +1,38 @@
+//12:50
+
 let productenInWagen = []
 const parentElement = document.querySelector("#koopProducten");
 const wagenTotaalPrijs = document.querySelector("#totaalPrijzen");
+const producten = document.querySelectorAll('.producten')
 
+
+function updateProductsInCart(product){
+    for(let i=0; i < updateProductenInWagen.length; i++){
+        if(productenInWagen[i].id == product.id){
+           productenInWagen[i].count += 1;
+           productenInWagen[i].price = productenInWagen[i].basePrice * productenInWagen[i].count;
+           return;
+        }
+    }
+}
+
+
+producten.forEach(product =>{
+    product.addEventListener('click', (e) =>{
+        if(e.target.classList.contains('addToCart')){
+            const productID = e.target.dataset.productID
+            const productNaam = product.quarySelector('').innerHTML;//voeg de class toe van de producten
+            const productPrijs = product.quarySelector('').innerHTML;//voeg de class toe van de prijs
+            let productToCart = {
+                name: productNaam,
+                id: productID,
+                count : 1,
+                price: +productPrijs,
+                basePrice : +productPrijs
+            }
+            updateProductsInCart(productToCart);
+            updateHTMLCart()
+
+        }
+    })
+})
