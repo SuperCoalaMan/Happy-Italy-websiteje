54a605e38956be64ced8aae417e3a98143821dbd
Valentijn :) <120188387+HerpieDerpieee@users.noreply.github.com>
2023-03-24 11:32:35 +0100

    bestelsysteem fix

--- Menu/js/bestel.js
+++ Menu/js/bestel.js
@@ -13,62 +13,69 @@ function addToCart(id){
         console.log(proinhoud)
         console.log(Naam)
         console.log(Prijs)
-     
+
         if(eerste==false){
-         let div = document.createElement("div")
-         div.innerHTML = '<div id="winkelinhoud'+id+'"><div class="tekst"><p id="naam"class="naam">' + Naam +'</p><p id="prijs'+id+'"class="prijs">'+ Prijs + '</p></div><div class="aantal"><p id="aantal'+id+'">'+aantal+'</p></div><div id="knoppen" class="knoppen"><div id="plus" class="plus" onclick="knop(`plus`,'+id+')">+</div><div id="min" class="min" onclick="knop(`min`,'+id+')">-</div></div></div>'    
-         document.getElementById("winkelwagen").appendChild(div)
-         console.log("di1")
-         array.push(id)
-         console.log(array)
-         eerste = true
-         }
+            let div = document.createElement("div")
+            div.innerHTML = '<div id="winkelinhoud'+id+'"><div class="tekst"><p id="naam"class="naam">' + Naam +'</p><p id="prijs'+id+'"class="prijs">'+ Prijs + '</p></div><div class="aantal"><p id="aantal'+id+'">'+aantal+'</p></div><div id="knoppen" class="knoppen"><div id="plus" class="plus" onclick="knop(`plus`,'+id+')">+</div><div id="min" class="min" onclick="knop(`min`,'+id+')">-</div></div></div>'    
+            document.getElementById("winkelwagen").appendChild(div)
+            console.log("di1")
+            array.push(id)
+            console.log(array)
+            eerste = true
+            }
         else{
-         for (let i = 0; i < array.length; i++){
-          if (array[i] == id){
-              let kaas = document.getElementById("aantal"+id);
-              let aantallen = kaas.innerHTML
-              aantallen++
-              let nieuwePrijs = parseFloat(Prijs) * aantallen
-              document.getElementById("aantal"+id).innerHTML = aantallen;
-              document.getElementById("prijs"+id).innerHTML = nieuwePrijs.toFixed(2)
-              console.log(nieuwePrijs)
-              return;
-          }
-         }
-         let div = document.createElement("div")
-         div.innerHTML = '<div id="winkelinhoud'+id+'"><div class="tekst"><p id="naam"class="naam">' + Naam +'</p><p id="prijs'+id+'"class="prijs">'+ Prijs + '</p></div><div class="aantal"><p id="aantal'+id+'">'+aantal+'</p></div><div id="knoppen" class="knoppen"><div id="plus" class="plus" onclick="knop(`plus`,'+id+')">+</div><div id="min" class="min" onclick="knop(`min`,'+id+')">-</div></div></div>'
-         document.getElementById("winkelwagen").appendChild(div)
-         console.log("di2")
-         array.push(id)
-         console.log(array)
+            for (let i = 0; i < array.length; i++){
+                if (array[i] == id){
+                    let kaas = document.getElementById("aantal"+id);
+                    let aantallen = kaas.innerHTML
+                    aantallen++
+                    let nieuwePrijs = parseFloat(Prijs) * aantallen
+                    document.getElementById("aantal"+id).innerHTML = aantallen;
+                    document.getElementById("prijs"+id).innerHTML = nieuwePrijs.toFixed(2)
+                    console.log(nieuwePrijs)
+                    return;
+                }
+            }
+            let div = document.createElement("div")
+            div.innerHTML = '<div id="winkelinhoud'+id+'"><div class="tekst"><p id="naam"class="naam">' + Naam +'</p><p id="prijs'+id+'"class="prijs">'+ Prijs + '</p></div><div class="aantal"><p id="aantal'+id+'">'+aantal+'</p></div><div id="knoppen" class="knoppen"><div id="plus" class="plus" onclick="knop(`plus`,'+id+')">+</div><div id="min" class="min" onclick="knop(`min`,'+id+')">-</div></div></div>'
+            document.getElementById("winkelwagen").appendChild(div)
+            console.log("di2")
+            array.push(id)
     } 
+    console.log(array);
 }
 
 function knop(welke,id){
-    console.log(array);
     console.log("ja", id)
     let Prijs = document.querySelector("#pro"+id+" :nth-child(2)").innerHTML;
     if(welke == "plus"){ 
         console.log("plus")
-    let kaas = document.getElementById("aantal"+id);
-    let aantallen = kaas.innerHTML
-    aantallen++
-    let nieuwePrijs = parseFloat(Prijs) * aantallen
-    document.getElementById("aantal"+id).innerHTML = aantallen;
-    document.getElementById("prijs"+id).innerHTML = nieuwePrijs.toFixed(2)
+        let kaas = document.getElementById("aantal"+id);
+        let aantallen = kaas.innerHTML
+        aantallen++
+        let nieuwePrijs = parseFloat(Prijs) * aantallen
+        document.getElementById("aantal"+id).innerHTML = aantallen;
+        document.getElementById("prijs"+id).innerHTML = nieuwePrijs.toFixed(2)
     }
     else if(welke == "min"){
         console.log("min")
         let kaas = document.getElementById("aantal"+id);
-    let aantallen = kaas.innerHTML
-    aantallen--
-    document.getElementById("aantal"+id).innerHTML = aantallen;
+        let aantallen = kaas.innerHTML
+        aantallen--
+        let nieuwePrijs = parseFloat(Prijs) * aantallen
+        document.getElementById("aantal"+id).innerHTML = aantallen;
+        document.getElementById("prijs"+id).innerHTML = nieuwePrijs.toFixed(2)
         if(aantallen == 0){
             const divje = document.getElementById("winkelinhoud"+id)
             console.log(divje)
             divje.remove()
             eerste = false
+
+            const index = array.indexOf(id);
+            if (index > -1) { // only splice array when item is found
+                array.splice(index, 1); // 2nd parameter means remove one item only
+            }
         }
-}
+    }
+    console.log(array);
 }
